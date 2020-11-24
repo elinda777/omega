1cf5a0b5b629093d574199e3059cf1c03b933c7b
elinda777 <73699772+elinda777@users.noreply.github.com>
2020-11-24 13:47:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+     <?php include "head.php"; ?>
+  </head>
+  <body class="app sidebar-mini">
+    <?php
+      include "navbar.php";
+      include "sidebar.php";
+      include "linkjs.php";
+    ?>
+  </body>
+</html>
